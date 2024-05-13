b9030df2ec448afb892489b10029779f2098f43a
TytanMikJas <mikjas02@gmail.com>
2024-05-13 13:28:17 +0200

    Clean error logging

---
+++ book-network/src/main/java/com/tytan/book/handler/BusinessErrorCode.java
@@ -0,0 +1,27 @@
+package com.tytan.book.handler;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+import static org.springframework.http.HttpStatus.*;
+
+@Getter
+public enum BusinessErrorCode {
+
+    NO_CODE(0, NOT_IMPLEMENTED, "No code"),
+    INCORRECT_CURRENT_PASSWORD(300, BAD_REQUEST, "The password provided is incorrect"),
+    NEW_PASSWORD_DOES_NOT_MATCH(301, BAD_REQUEST, "The new password does not match"),
+    ACCOUNT_LOCKED(302, FORBIDDEN, "User account is locked"),
+    ACCOUNT_DISABLED(303, FORBIDDEN, "User account is disabled"),
+    BAD_CREDENTIALS(304, UNAUTHORIZED, "Login and / or password is incorrect ");
+
+    private final int code;
+    private final String description;
+    private final HttpStatus httpStatus;
+
+    BusinessErrorCode(int code, HttpStatus httpStatus, String description) {
+        this.code = code;
+        this.httpStatus = httpStatus;
+        this.description = description;
+    }
+}
