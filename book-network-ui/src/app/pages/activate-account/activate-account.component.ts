1a24d394d52a6c1b133f0d74d5e2c4d737e06eb6
TytanMikJas <mikjas02@gmail.com>
2024-06-16 17:44:06 +0200

    finished account activation impl

--- book-network-ui/src/app/pages/activate-account/activate-account.component.ts
+++ book-network-ui/src/app/pages/activate-account/activate-account.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {Router} from "@angular/router";
+import {AuthenticationService} from "../../services/services/authentication.service";
 
 @Component({
   selector: 'app-activate-account',
@@ -6,5 +8,37 @@ import { Component } from '@angular/core';
   styleUrl: './activate-account.component.scss'
 })
 export class ActivateAccountComponent {
+  message: string = '';
+  isOkay: boolean = true;
+  submitted: boolean = false;
 
+  constructor(
+    private router: Router,
+    private authService: AuthenticationService,
+  ) {}
+
+  onCodeCompleted(token: string) {
+    this.confirmAccount(token);
+  }
+
+  redirectToLogin() {
+    this.router.navigate(['login'])
+  }
+
+  private confirmAccount(token: string) {
+    this.authService.confirm({
+      token
+    }).subscribe({
+      next: () => {
+        this.message = 'Account activated successfully\nProceed to login';
+        this.submitted = true;
+        this.isOkay = true;
+      },
+      error: (error) => {
+        this.message = 'Token has been expired or invalid\nA new token has been sent to your email address\nPlease try again'
+        this.submitted = true;
+        this.isOkay = false;
+      }
+    })
+  }
 }
